695c04dcf09fdbb9c87e0e08af4dc1258cba4fee
liutao12138 <1043783381@qq.com>
2020-05-30 01:20:15 +0800

    1

--- canvas/src/app/demo/demo1/demo1.component.ts
+++ canvas/src/app/demo/demo1/demo1.component.ts
@@ -10,6 +10,13 @@ export class Demo1Component implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.init();
+  }
+
+  private init() {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    ctx.strokeRect(60, 60, 60, 60);
   }
 
 }
